584288b426943e5891014ea047744df93e5156e8
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2022-10-28 23:15:20 -0600

    Added resume route

--- django_personal_site/autobiography/urls.py
+++ django_personal_site/autobiography/urls.py
@@ -1,8 +1,7 @@
-from xml.etree.ElementInclude import include
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='autobiography-home'),
+    path('resume/', views.resume, name='autobiography-resume'),
 ]
--- django_personal_site/autobiography/views.py
+++ django_personal_site/autobiography/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Home</h1>')
 
+def resume(request):
+    return HttpResponse('<h1>Resume</h1>')
