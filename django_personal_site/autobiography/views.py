5e505b203ad5dfd318058e1ee5a5f55ee6f4b7a3
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2022-10-28 23:33:19 -0600

    Added home.html template

--- django_personal_site/autobiography/views.py
+++ django_personal_site/autobiography/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'autobiography/home.html')
 
 def resume(request):
     return HttpResponse('<h1>Resume</h1>')
