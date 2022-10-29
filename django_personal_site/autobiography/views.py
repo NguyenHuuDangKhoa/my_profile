e9bf485fefdb25073bf35f5bc74c83d22b966202
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2022-10-28 23:50:19 -0600

    Updated base.html template'

--- django_personal_site/autobiography/views.py
+++ django_personal_site/autobiography/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'autobiography/home.html')
+    return render(request, 'autobiography/home.html', {'title': "Home"})
 
 def resume(request):
-    return render(request, 'autobiography/resume.html')
+    return render(request, 'autobiography/resume.html', {'title': "Resume"})
